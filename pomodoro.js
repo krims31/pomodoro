971c120736f3c9b76df92858f4696ee62f92538b
krims31 <vegor405@gmail.com>
2026-03-10 17:58:59 +0300

    add music and change range

--- pomodoro.js
+++ pomodoro.js
@@ -1,49 +1,66 @@
-import music from "./music.js";
-const displayTime = document.querySelector('.time h1');
-const startBtn = document.querySelector('.start button');
-const resetBtn = document.querySelector('.reset button');
-const dropdown = document.querySelector('.dropdown');
+const displayTime = document.querySelector('.time h1')
+const startBtn = document.querySelector('.start button')
+const resetBtn = document.querySelector('.reset button')
+const dropdown = document.querySelector('.dropdown')
+const musicBtn = document.querySelector('.play-btn')
+const rangeMusic = document.querySelector('.range-music input')
 
-let time = 25 * 60;
-let timerInterval;
+let time = 25 * 60
+let timerInterval
+
+const audio = new Audio(
+	'assets/music/Ghostrifter-Official-Purple-Dream(chosic.com).mp3'
+)
+
+rangeMusic.addEventListener('input', () => {
+	if (audio) {
+		audio.volume = rangeMusic.value / 100
+	}
+})
 
 function dropDownToggle() {
 	if (dropdown.classList.contains('active')) {
-		dropdown.classList.remove('active');
+		dropdown.classList.remove('active')
 	} else {
-		dropdown.classList.add('active');
+		dropdown.classList.add('active')
 	}
-	
 }
 function displayUpdate() {
-	const minutes = Math.floor(time / 60);
-	const seconds = time % 60;
+	const minutes = Math.floor(time / 60)
+	const seconds = time % 60
 
-	displayTime.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+	displayTime.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`
 }
 
 function start() {
 	if (timerInterval) {
-		clearInterval(timerInterval);
+		clearInterval(timerInterval)
 	}
 
 	timerInterval = setInterval(() => {
-		time--;
-		displayUpdate();
+		time--
+		displayUpdate()
 
 		if (time <= 0) {
-			clearInterval(timerInterval);
-			music.play();
+			clearInterval(timerInterval)
+			music.play()
 		}
-	}, 1000);
+	}, 1000)
 }
 
 function reset() {
-	clearInterval(timerInterval);
-	time = 25 * 60;
-	displayUpdate();
+	clearInterval(timerInterval)
+	time = 25 * 60
+	displayUpdate()
 }
 
-startBtn.addEventListener('click', start);
-resetBtn.addEventListener('click', reset);
-dropdown.addEventListener('click', dropDownToggle);
+startBtn.addEventListener('click', start)
+resetBtn.addEventListener('click', reset)
+dropdown.addEventListener('click', dropDownToggle)
+musicBtn.addEventListener('click', () => {
+	if (audio.paused) {
+		audio.play()
+	} else {
+		audio.pause()
+	}
+})
